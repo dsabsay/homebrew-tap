dbc3d6fec113d7f0c2fbf5ba77aeb6c2a34fbcf7
dsabsay <danielrsabsay@gmail.com>
2020-07-18 15:15:38 -0700

    add version

--- Formula/lazylit.rb
+++ Formula/lazylit.rb
@@ -3,6 +3,7 @@ class Lazylit < Formula
   homepage "https://github.com/dsabsay/lazylit"
   url "https://github.com/dsabsay/lazylit/releases/download/0.1.0/lazylit-0.1.0.darwin-amd64.tar.gz"
   sha256 "709b1053654903ad051602c2d925c2c2be23419ffc5c0a1da7efecaff099790e"
+  version "0.1.0"
 
   bottle :unneeded
 
