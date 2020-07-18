b9b80dd504cbfc0c96cffc237b9266d527b3726b
dsabsay <danielrsabsay@gmail.com>
2020-07-18 16:19:17 -0700

    version 0.2.1

--- Formula/lazylit.rb
+++ Formula/lazylit.rb
@@ -1,9 +1,10 @@
 class Lazylit < Formula
-  desc "Generate source code documentation as static web pages."
+  desc "Generate source code documentation as static web pages"
   homepage "https://github.com/dsabsay/lazylit"
-  url "https://github.com/dsabsay/lazylit/releases/download/0.1.0/lazylit-0.1.0.darwin-amd64.tar.gz"
-  sha256 "709b1053654903ad051602c2d925c2c2be23419ffc5c0a1da7efecaff099790e"
-  version "0.1.0"
+  url "https://github.com/dsabsay/lazylit/releases/download/0.2.1/lazylit-0.2.1.darwin-amd64.tar.gz"
+  version "0.2.1"
+  sha256 "51c489ee33777dcd52e0349d607eea259f840d90c692d7e98bd0b8cef731af10"
+  license "MIT"
 
   bottle :unneeded
 
